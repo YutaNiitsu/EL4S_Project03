2b2340133d3186f184e915cd34dbcf7c3bfc1524
HarunoSatou <haruno620@icloud.com>
2023-06-24 14:27:23 +0900

    <変更>UI変更/ゲームシーン追加

--- Assets/UI/CharaUI.cs
+++ Assets/UI/CharaUI.cs
@@ -12,9 +12,9 @@ public class CharaUI : MonoBehaviour
     //[SerializeField] private float _goalPos;
     [SerializeField] private GameObject _road;
     [SerializeField] private float _goalLength;
-    [SerializeField] private GameObject _player;
-    [SerializeField] private GameObject _enemy1;
-    [SerializeField] private GameObject _enemy2;
+    [SerializeField] private Player _player;
+    [SerializeField] private Player _enemy1;
+    [SerializeField] private Player _enemy2;
     [SerializeField] private GameObject _playerIcon;
     [SerializeField] private GameObject _enemy1Icon;
     [SerializeField] private GameObject _enemy2Icon;
@@ -30,9 +30,9 @@ public class CharaUI : MonoBehaviour
     void Update()
     {
         //キャラクター内のポジションをもらう
-        float playerPos = _player.transform.position.x/*TODO:内部パラメータをもらう*/ * _oneMoveLength + _startPos;
-        float enemy1Pos = _enemy1.transform.position.x/*TODO:内部パラメータをもらう*/ * _oneMoveLength + _startPos;
-        float enemy2Pos = _enemy2.transform.position.x/*TODO:内部パラメータをもらう*/ * _oneMoveLength + _startPos;
+        float playerPos = _player.GetMoveDistance()/*TODO:内部パラメータをもらう*/ * _oneMoveLength + _startPos;
+        float enemy1Pos = _enemy1.GetMoveDistance()/*TODO:内部パラメータをもらう*/ * _oneMoveLength + _startPos;
+        float enemy2Pos = _enemy2.GetMoveDistance()/*TODO:内部パラメータをもらう*/ * _oneMoveLength + _startPos;
 
         RectTransform playerIconPos = _playerIcon.GetComponent<RectTransform>();
         RectTransform enemy1IconPos = _enemy1Icon.GetComponent<RectTransform>();
