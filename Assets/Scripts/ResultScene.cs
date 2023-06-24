99f9793308d772baf3c30c19691833623062b9a8
UchimuraYousuke <yousukeuchimura8113@gmail.com>
2023-06-24 16:14:55 +0900

    no message

--- Assets/Scripts/ResultScene.cs
+++ Assets/Scripts/ResultScene.cs
@@ -7,11 +7,25 @@ public class ResultScene : MonoBehaviour
 {
     [SerializeField] private Color fadeColor = Color.black;
     [SerializeField] private float fadeSpeedMultiplier = 10.0f;
+    ClickSE click;
+    private int winlose;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        click = this.GetComponent<ClickSE>();
+        //click.PlaySE(1);
+        //click.PlaySE(2);
+        winlose = Player.GetPlayerRank();
+        if (winlose == 0)
+        {
+            click.PlaySE(1);
+        }
+        else
+        {
+            click.PlaySE(2);
+        }
+
     }
 
     // Update is called once per frame
@@ -22,12 +36,13 @@ public class ResultScene : MonoBehaviour
 
     public void OnClickReTryGameButton()
     {
+        click.PlaySE(0);
         Initiate.Fade("GameScene", fadeColor, fadeSpeedMultiplier);
     }
 
     public void OnClickBackTheTitleButton()
     {
+        click.PlaySE(0);
         Initiate.Fade("Title", fadeColor, fadeSpeedMultiplier);
-
     }
 }
