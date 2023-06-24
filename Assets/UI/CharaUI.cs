422f666f93860dda1a01c4741c9ec9ff2bd9fa61
HarunoSatou <haruno620@icloud.com>
2023-06-24 16:11:14 +0900

    <変更>プレイヤーたち追加

--- Assets/UI/CharaUI.cs
+++ Assets/UI/CharaUI.cs
@@ -8,22 +8,24 @@ public class CharaUI : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    [SerializeField] private float _startPos;
+    private float _startPos;
     //[SerializeField] private float _goalPos;
     [SerializeField] private GameObject _road;
     [SerializeField] private float _goalLength;
     [SerializeField] private Player _player;
-    [SerializeField] private Player _enemy1;
-    [SerializeField] private Player _enemy2;
+    [SerializeField] private EnemyPlayer _enemy1;
+    [SerializeField] private EnemyPlayer _enemy2;
     [SerializeField] private GameObject _playerIcon;
     [SerializeField] private GameObject _enemy1Icon;
     [SerializeField] private GameObject _enemy2Icon;
+
 
     private float _oneMoveLength;
 
     void Start()
     {
         _oneMoveLength = _road.GetComponent<RectTransform>().sizeDelta.x / _goalLength;
+        _startPos = _road.GetComponent<RectTransform>().sizeDelta.x * 0.5f * -1.0f;
     }
 
     // Update is called once per frame
@@ -38,7 +40,7 @@ public class CharaUI : MonoBehaviour
         RectTransform enemy1IconPos = _enemy1Icon.GetComponent<RectTransform>();
         RectTransform enemy2IconPos = _enemy2Icon.GetComponent<RectTransform>();
         playerIconPos.anchoredPosition = new Vector2(playerPos, playerIconPos.anchoredPosition.y);
-        enemy1IconPos.anchoredPosition = new Vector2(playerPos, playerIconPos.anchoredPosition.y);
-        enemy2IconPos.anchoredPosition = new Vector2(playerPos, playerIconPos.anchoredPosition.y);
+        enemy1IconPos.anchoredPosition = new Vector2(enemy1Pos, enemy1IconPos.anchoredPosition.y);
+        enemy2IconPos.anchoredPosition = new Vector2(enemy2Pos, enemy2IconPos.anchoredPosition.y);
     }
 }
