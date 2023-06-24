54ad33c64b3814c0363ff0f0a223c8ddc1628c80
UchimuraYousuke <yousukeuchimura8113@gmail.com>
2023-06-24 16:57:55 +0900

    no message

---
+++ Assets/BackGround/Scripts/TGoalMana.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TGoalMana : MonoBehaviour
+{
+    [SerializeField][Tooltip("ç≈ëÂÉQÅ[ÉÄéûä‘")] private float MaXTime = 60.0f;
+    [SerializeField] private StartMana StartMana;
+    private float nowatime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nowatime = 0.0f;
+        StartCoroutine(CheckTime());
+    }
+
+    IEnumerator CheckTime()
+    {
+        nowatime += Time.deltaTime;
+        yield return new WaitForSeconds(MaXTime+StartMana.Delay);
+        StartMana.StopBackGround();
+    }
+
+    public float TimeRate()
+    {
+        return nowatime / MaXTime+StartMana.Delay;
+    }
+   
+}
